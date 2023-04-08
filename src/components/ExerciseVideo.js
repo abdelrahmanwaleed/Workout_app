33044e784c8a794fe2b2664b7b745ba5d69358ef
Abdelrahman Waleed <abdelrahmanmoali@gmail.com.com>
2023-04-08 14:59:43 +0200

    second update2

--- src/components/ExerciseVideo.js
+++ src/components/ExerciseVideo.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 const ExerciseVideo = ({ exerciseVideo, name }) => {
-  if (!exerciseVideo.lenght) return "ya...isloading";
-  console.log(exerciseVideo);
+  if (!exerciseVideo.length) return "ya...isloading";
 
   return (
     <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
@@ -24,7 +23,7 @@ const ExerciseVideo = ({ exerciseVideo, name }) => {
           gap: { lg: "110px", xs: "0" },
         }}
       >
-        {ExerciseVideo?.slice(0, 3).map((item, index) => (
+        {exerciseVideo?.slice(0, 3).map((item, index) => (
           <a
             key={index}
             className="flex flex-col gap-6 w-[387px] h-[381px] no-underline"
